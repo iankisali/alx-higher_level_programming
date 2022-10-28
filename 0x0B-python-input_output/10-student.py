a5b4fa5979a87885d3497419e01538fe4363c233
iankisali <iankisali@gmail.com>
2022-10-28 01:11:24 -0700

    retrieves a dictionary representation of a Student based task 8

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize new student
+        Args:
+            first_name: first name
+            last_name: last name
+            age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get dict representation of student
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
